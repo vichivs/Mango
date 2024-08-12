4b485ffa38f2d9ec99f649c6f62b25d405aee2f3
Vichitra Veer Singh <Vichitra.veer.singh@neotechnologysolutions.com>
2024-08-12 23:30:49 +0530

    Added AuthRoles and Login/Regiseter in UI

--- Mango.Services.AuthAPI/Service/AuthService.cs
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -20,6 +20,23 @@ namespace Mango.Services.AuthAPI.Service
             _roleManager = roleManager;
             _jwtTokenGenerator = jwtTokenGenerator;
         }
+
+        public async Task<bool> AssignRole(string email, string roleName )
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
+            if(user != null )
+            {
+                if(! await _roleManager.RoleExistsAsync(roleName))
+                {
+                    //create role if not exists
+                    await _roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+                await _userManager.AddToRoleAsync(user, roleName);
+                return true;
+            }
+            return false;
+        }
+
         public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
         {
             var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower());
---
+++ Mango.Web/Service/AuthService.cs
@@ -0,0 +1,44 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        public readonly IBaseService _baseService;
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = registrationRequestDto,
+                Url = AuthAPIBase + "/api/auth/AssignRole"
+            });
+        }
+
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = loginRequestDto,
+                Url = AuthAPIBase + "/api/auth/login"
+            });
+        }
+
+        public async Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = registrationRequestDto,
+                Url = AuthAPIBase + "/api/auth/register"
+            });
+        }
+    }
+}
