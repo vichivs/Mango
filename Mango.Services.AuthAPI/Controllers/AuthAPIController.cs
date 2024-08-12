dcd41ae592f5482fc590a4362b93cd670543a9c8
Vichitra Veer Singh <Vichitra.veer.singh@neotechnologysolutions.com>
2024-08-12 12:32:59 +0530

    Applied Authentication endpoints

---
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,40 @@
+﻿using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        protected ResponseDto _response;
+
+        public AuthAPIController(IAuthService authService)
+        {
+            _authService = authService;
+            _response = new ();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationRequestDto model)
+        {
+            var errorMessage = await _authService.Register(model);
+            if(!string.IsNullOrEmpty(errorMessage))
+            {
+                _response.IsSuccess = false;
+                _response.Message = errorMessage;
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
